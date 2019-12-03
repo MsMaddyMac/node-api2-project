64a749ad13212ebb82a66d3b6e1350adddfb3a30
Maddy <mmcintosh2k14@gmail.com>
2019-12-03 16:07:31 -0500

    creates GET endpoint for retrieving post object by specified ID

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.use(express.json());
 
+// gets an array of posts contained in the database.
 router.get('/', (req, res) => {
     Posts.find(req.query)
     .then(posts => {
@@ -14,10 +15,26 @@ router.get('/', (req, res) => {
     .catch(err => {
         console.log(err);
         res.status(500).json({ 
-            message: 'Error retrieving posts!' 
+            message: 'The posts information could not be retrieved!' 
         });
     });
 });
+
+// gets post object by specified ID
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+    .then(post => {
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'The post with the specified ID does not exist.' })
+        }
+    })
+    .catch(err => {
+        console.log('ID not valid', err);
+        res.status(500).json({ error: 'The post information could not be retrieved.' });
+    });
+})
 
 //export default 
 module.exports = router;
